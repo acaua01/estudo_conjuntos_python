0eb3f5a2aa8faa71f190adb4a4654fef9f588166
Acauã Vieira Payão <136847436+acaua01@users.noreply.github.com>
2023-06-23 15:55:42 -0300

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,39 @@
+"""
+Programa conjuntos
+Descrição: Este programa apresenta a estrutura conjuntos em python para fins didáticos
+Autor: Acauã Vieira Payão
+Data: 23/06/2023
+Versão:0.0.1
+"""
+
+
+# Atribuição de variáveis
+
+lista = [1,2,3,3,4,5,3,6,7,8,8,8,54,3,3,2,2,2,47,32]
+
+conjunto_1 = set(lista)
+
+conjunto_2 = {1,3,3,5,5,0,0,23,-2}
+
+
+
+# Entrada de dados
+
+
+
+
+# Processamento de dados
+
+conjunto_2.add(49)
+
+conjunto_2.remove(0)
+
+
+# Saída de dados
+
+print(conjunto_1)
+
+print(lista)
+
+print(conjunto_2)
+
